8ae845635249d88b976353b4c3edbf5a7638cc9b
Ayelen <ayelennuno@gmail.com>
2022-10-18 13:02:15 +0200

    esuqema inicial preProtocol, y PostProtocol

--- p2/CodigoEjemploEsqueletoP2/practica2/ra/ra.go
+++ p2/CodigoEjemploEsqueletoP2/practica2/ra/ra.go
@@ -21,16 +21,18 @@ type Request struct{
 type Reply struct{}
 
 type RASharedDB struct {
-    OurSeqNum   int
-    HigSeqNum   int
-    OutRepCnt   int
-    ReqCS       boolean
-    RepDefd     int[]
-    ms          *MessageSystem
-    done        chan bool
-    chrep       chan bool
-    Mutex       sync.Mutex // mutex para proteger concurrencia sobre las variables
+    OurSeqNum   int             // Nuestro numero de secuencia (reloj)
+    HigSeqNum   int             // Mayor numero de secuencia (reloj)
+    OutRepCnt   int             // Numero de procesos pendientes por confirmar acceso a SC
+    ReqCS       boolean         // cs_state = si etamos en SC
+    RepDefd     int[]           // Procesos que esperan nuestra confirmacion
+    ms          *MessageSystem  
+    done        chan bool       // avisamos al salir de la SC  
+    chrep       chan bool       // chan replay (para la respuesta) y saber si tenemos permiso para acceder a la SC 
+    Mutex       sync.Mutex      // mutex para proteger concurrencia sobre las variables
     // TODO: completar
+    Me          int             // Mi número de nodo 
+    N           int             // numero de nodoso en la red
 }
 
 
@@ -45,15 +47,44 @@ func New(me int, usersFile string) (*RASharedDB) {
 //Pre: Verdad
 //Post: Realiza  el  PreProtocol  para el  algoritmo de
 //      Ricart-Agrawala Generalizado
+//El preprotocol para un Proceso Pi consiste en enviar a los N - 1 
+//procesos distribuidos una peticion de acceso a la seccion critica. 
+//Cuando se reciba la respuesta en (5) de los N-1 entonces el Proceso 
+//Pi accede a la seccion critica.
+
 func (ra *RASharedDB) PreProtocol(){
-    // TODO completar
+    // cs_state <- trying 
+    ra.Mutex.Lock() 
+    ra.ReqCS = true                      // indicamos que queremos acceder a la SC 
+    ra.OurSeqNum = ra.HigSeqNum + 1      // aumentamos el reloj interno 
+    ra.Mutex.Unlock() 
+    
+    ra.OutRepCnt = ra.N - 1              // Numero de procesos que confirman la entrada a SC
+    for j := 1; j < ra.N; j++ {
+        if j != ra.Me {
+            // send (enviamos la peticion de acceso a la SC)
+        }
+	}
+
+    for ra.OutRepCnt != 0 {
+        <-ra.chrep                          // esperamos respuesta (leer del canal)
+        ra.OutRepCnt =  ra.OutRepCnt - 1    // decrementas contador de numero de procesos que faltan 
+    }   
+    ra.ReqCS = false            // puede que no vaya aqui :/ (igual al inicio del post)-------------------------------------------------------------------
 }
 
 //Pre: Verdad
 //Post: Realiza  el  PostProtocol  para el  algoritmo de
 //      Ricart-Agrawala Generalizado
+// Enviamos permiso a los procesos que teniamos esperando la confirmacion
+// una vez salimos de nuestra SC
 func (ra *RASharedDB) PostProtocol(){
-    // TODO completar
+    for j := 1; j < ra.N; j++ {
+        if ra.RepDefd[j] {
+            ra.RepDefd[j] = false           
+            // send nuestro permiso para que j entre en SC
+        }
+	}
 }
 
 func (ra *RASharedDB) Stop(){
