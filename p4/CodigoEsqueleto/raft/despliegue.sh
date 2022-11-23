9413258b701cdb700eb258d0c23df2ad27dd2168
Ayelen <ayelennuno@gmail.com>
2022-11-23 10:31:17 +0100

    a

--- p4/CodigoEsqueleto/raft/despliegue.sh
+++ p4/CodigoEsqueleto/raft/despliegue.sh
@@ -5,8 +5,8 @@ go build pkg/cltraft/main.go
 #go run cmd/srvraft/main.go 0 127.0.0.1:29001
 go run cmd/srvraft/main.go 0 127.0.0.1:29001 127.0.0.1:29002&
 go run cmd/srvraft/main.go 1 127.0.0.1:29001 127.0.0.1:29002&
-#go run cmd/srvraft/main.go 0 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003&
-#go run cmd/srvraft/main.go 1 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003&
-#go run cmd/srvraft/main.go 2 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003&
+#go run cmd/srvraft/main.go 0 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003
+#go run cmd/srvraft/main.go 1 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003
+#go run cmd/srvraft/main.go 2 127.0.0.1:29001 127.0.0.1:29002 127.0.0.1:29003
 
 go run pkg/cltraft/main.go 127.0.0.1:3000 127.0.0.1:3001 127.0.0.1:3002
